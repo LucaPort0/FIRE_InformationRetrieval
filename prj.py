767e216df8fe280b5dbc5529b094d6f0f5d15819
Luca Porto <64616137+LucaPort0@users.noreply.github.com>
2020-07-13 00:22:07 -0300

    Almost final version, but functional
    Getting results and plotting in a graph, must save the indexed files for future runnings to save time and create a loop to check different queries or operations

--- prj.py
+++ prj.py
@@ -5,9 +5,17 @@ import time
 import collections
 import math
 
+import pickle
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+nltk.download('stopwords')
+
+import seaborn as sns
 import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+sns.set_style('whitegrid')
 
 # Printing function for nested dictionaries
 def printTermsDict(d):
@@ -53,20 +61,20 @@ def wordStemmer(termDict):
     return stemmed
 
 #Normalizing and tokenizing function for the files
-def normalizeFilesText(list, path):
+def normalizeFilesText(tokens, path):
     with open(path, "r") as f:
-        list = [x.strip() for x in f.read().split()]
+        tokens = [x.strip() for x in f.read().split()]
         
-    for i in range (len(list)):
-        if i != 1: #Skiping the first element(DocID) in the list
-            list[i] = re.sub(u'[^a-zA-ZáéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕçÇ<>_]', '', list[i]) #Removing punctuation
-        list[i] = re.sub("[<].*?[>]", "", list[i]) #Removing everything between <>
-        list[i] = list[i].lower() #lowercasing elements
+    for i in range (len(tokens)):
+        if i != 1: #Skiping the first element(DocID) in the tokens
+            tokens[i] = re.sub(u'[^a-zA-ZáéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕçÇ<>_]', '', tokens[i]) #Removing punctuation
+        tokens[i] = re.sub("[<].*?[>]", "", tokens[i]) #Removing everything between <>
+        tokens[i] = tokens[i].lower() #lowercasing elements
 
-    while '' in list: #Remove null elements in the list 
-        list.remove('')
+    while '' in tokens: #Remove null elements in the tokens 
+        tokens.remove('')
 
-    return list
+    return tokens
 
 # Function that read the query file and index it in a dictionary
 def readQueries(path, d):
@@ -78,9 +86,9 @@ def readQueries(path, d):
     getID = "<num>(.*?)</num>" # Finding strings between <num>, in this case, the ids
     ids = re.findall(getID, text) 
 
-    getQuery = "<narr>(.*?)</narr>"
+    getQuery = "<title>(.*?)</title>"
     qrs = re.findall(getQuery, text)
-    
+   
     for i in range(len(ids)):
         qrs[i] = re.sub(u'[^a-zA-ZáéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕçÇ<>_]', ' ', qrs[i]) # Normalizing
         qrs[i] = qrs[i].lower() # Lowercasing
@@ -100,11 +108,11 @@ def createNestedDict(d, tokens):
 
         if tokens[i] not in d: # If the word is not in the dictionary
             d[tokens[i]] = {}  # A dictionary of that word is created, with the doc id and frequency 
-            dict = tokens[i]
-            dict = {
+            termDict = tokens[i]
+            termDict = {
                 id : 1
             } 
-            d[tokens[i]] = dict
+            d[tokens[i]] = termDict
         else:
             if id in d[tokens[i]]: # If it is the same document 
                 d[tokens[i]][id] += 1 
@@ -134,11 +142,9 @@ def scan_folder(parent, tokens, d):
 def tfIdf(term, termDict, nDocs, t):
     w = 0
     nt = len(termDict[term]) # Number of documents with "term"
-
     w = ((1 + math.log2(t))) * (math.log2(nDocs/nt))
 
     return w
-
 
 # Vector Model function that returns the most relevant documents for the given query
 def vectorModel(termDict, queryDict, queryID, nDocs, pct):
@@ -170,19 +176,17 @@ def vectorModel(termDict, queryDict, queryID, nDocs, pct):
     scores = sorted(scores.items(), key=lambda x: x[1], reverse = True)
 
     minValue = scores[0][1]
-
     minValue = minValue * pct
-
     i = 0
     
     while(scores[i][1] > minValue):
         i += 1
     
-    print(i)
+    print(i, 'Arquivos recuperados')
     return dict(scores[:i])
 
 # Function to evaluate the results
-def evaluate(path, id, score):
+def evaluate(path, id, score, method):
     d = {}
     union = 0
     aux1 = 0
@@ -207,23 +211,17 @@ def evaluate(path, id, score):
         else:
             aux2 += 1
 
-    if len(score) == 0:
-        map = 0
-        precision = 0
-        recall = 0
-        f1 = 0
-    else:
-        map = (res/len(score))
-        recall = (union/len(d))
-        precision = (union/len(score))
+    recall = (union/len(d))
+    precision = (union/len(score))
+    if precision and recall:
         f1 = 2 * ((precision*recall)/(precision + recall))
-    
+    else:
+        f1 = 0
     print('Recall:', recall)
     print('Precision:', precision)
     print('F1:', f1)
-    print('Map:', map)
 
-    return d
+    return d, {'Method': method, 'Precision': precision, 'Recall': recall, 'F1': f1}
 
 ###################################    MAIN    ###################################################
 
@@ -237,66 +235,61 @@ scores = {} # Vetor de scores
 evaluation = {}
 stop = stopwords.words('english')
 
+print('Insira o path do diretorio TELEGRAPH_UTF8')
+print('EX: "/home/user/Desktop/FIRE2010/en.doc.2010/TELEGRAPH_UTF8" ')
+files_path = input()
 
-#print('Insira o path do diretorio TELEGRAPH_UTF8')
-#print('EX: "/home/user/Desktop/FIRE2010/en.doc.2010/TELEGRAPH_UTF8" ')
-#files_path = input()
-
-#print('Insira o path do arquivo com as queries')
-#print('EX: "/home/user/Desktop/FIRE2010/en.topics.76-125.2010.txt" ')
-#queries_path = input()
-
+print('Insira o path do arquivo com as queries')
+print('EX: "/home/user/Desktop/FIRE2010/en.topics.76-125.2010.txt" ')
+queries_path = input()
 print('Lendo e indexando os arquivos e queries(pode levar algum tempo)')
-terms = scan_folder("/home/user/Desktop/TopBD/Projeto/FIRE2010/en.doc.2010/TELEGRAPH_UTF8", tokens, terms)  # Insert parent directory's path
-queries = readQueries("/home/user/Desktop/TopBD/Projeto/FIRE2010/en.topics.76-125.2010.txt", queries)
+
+terms = scan_folder(files_path, tokens, terms)  # Insert parent directory's path
+queries = readQueries(queries_path, queries)
 print('Concluido\n')
 
-print('Insira o número da comparação a ser feita:\n1 Indexacao com e sem stopwords\n2 Indexação com e sem radicalizacao\n0 Para sair\n')
+print('Insira o número da comparação a ser feita:\n1 Indexacao com e sem stopwords\n2 Indexação com e sem radicalizacao\n')
 operation = input()
 
 print('\nEscolha a query a ser analisada(76 a 125)\n')
 q = input()
 
-while(operation != 0):
-    termos = terms.copy()
-    buscas = queries.copy()
-    print('Analise original\n')
-    scores = vectorModel(termos, buscas, q, nDocs, 0.80)
-    evaluation = evaluate("/home/user/Desktop/TopBD/Projeto/FIRE2010/en.qrels.76-125.2010.txt", int(q), scores)
-    print(buscas[q])
+print('Analise original\n')
+scores = vectorModel(terms, queries, q, nDocs, 0.7)
+evaluation, x1 = evaluate("/home/user/Desktop/TopBD/Projeto/FIRE2010/en.qrels.76-125.2010.txt", int(q), scores, 'Com Stopwords')
 
+if operation == '1': # Analise sem Stopwords
+    print('\nAnalise sem Stopwords\n')
+    terms = stopWords(terms)
+    aux = queries[q]
+    aux2 = [word for word in aux if not word in stop]
+    queries[q] = aux2
+    scores = vectorModel(terms, queries, q, nDocs, 0.8)
+    evaluation, x2 = evaluate("/home/user/Desktop/TopBD/Projeto/FIRE2010/en.qrels.76-125.2010.txt", int(q), scores, 'Sem Stopwords')
 
-    if operation == '1': # Analise sem Stopwords
-        print('\nAnalise sem Stopwords\n')
-        termos = stopWords(termos)
-        aux = buscas[q]
-        aux2 = [word for word in aux if not word in stop]
-        buscas[q] = aux2
-        scores = vectorModel(termos, buscas, q, nDocs, 0.80)
-        evaluation = evaluate("/home/user/Desktop/TopBD/Projeto/FIRE2010/en.qrels.76-125.2010.txt", int(q), scores)
+    y = pd.DataFrame([x1, x2]).set_index('Method')
+    y.plot(kind = 'bar', rot = 0, figsize=(16,8))
+    axis = plt.gca
 
-    elif operation == '2': # Analise com Stemming
-        print('\nAnalise com Stemming')
-        ps = PorterStemmer()
-        termos = stopWords(termos)
-        termos = wordStemmer(termos)
-        aux = buscas[q]
-        aux1 = [word for word in aux if not word in stop]
-        buscas[q] = aux1
-        aux2 = []
-        for word in buscas[q]:
-            aux = ps.stem(word)
-            aux2.append(aux)
+    plt.show()
 
-        buscas[q] = aux2
-        scores = vectorModel(termos, buscas, q, nDocs, 0.70)
-        print(scores)
-        evaluation = evaluate("/home/user/Desktop/TopBD/Projeto/FIRE2010/en.qrels.76-125.2010.txt", int(q), scores)
+elif operation == '2': # Analise com Stemming
+    print('\nAnalise com Stemming')
+    ps = PorterStemmer()
+    terms = wordStemmer(terms)
+    aux2 = []
+    for word in queries[q]:
+        aux = ps.stem(word)
+        aux2.append(aux)
 
-    print('Insira o número da comparação a ser feita:\n1 Indexacao com e sem stopwords\n2 Indexação com e sem radicalizacao\n0 Para sair\n')
-    operation = input()
+    queries[q] = aux2
+    scores = vectorModel(terms, queries, q, nDocs, 0.70)
+    evaluation, x2 = evaluate("/home/user/Desktop/TopBD/Projeto/FIRE2010/en.qrels.76-125.2010.txt", int(q), scores, 'Com Stemming')
 
-    print('\nEscolha a query a ser analisada(76 a 125)\n')
-    q = input()
+    y = pd.DataFrame([x1, x2]).set_index('Method')
+    y.plot(kind = 'bar', rot = 0, figsize=(16,8))
+    axis = plt.gca
+
+    plt.show()
 
 print(' %s seconds ' % (time.time() - start_time))
